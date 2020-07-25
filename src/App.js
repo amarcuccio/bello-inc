b73859790be432e01b87d10f1efbe435c251fca3
Andrew Marcuccio <marcuccio.andrew@gmail.com>
2020-07-25 16:16:52 -0400

    Switched over to MUI.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 
 const App = () => {
-  return <h1>app</h1>;
+  return (
+    <>
+      <CssBaseline />
+      <Typography variant="h6">Bello & Co Inc.</Typography>
+    </>
+  );
 };
 
 export default App;
