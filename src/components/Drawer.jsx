aa45e4c618ba051be90d5c95dbc1d867667ae351
Andrew Marcuccio <marcuccio.andrew@gmail.com>
2020-07-26 16:14:29 -0400

    Slight refactor.

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Slide from '@material-ui/core/Slide';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import Slide from '@material-ui/core/Slide';
 import { useTheme } from '@material-ui/core/styles';
 
 const Drawer = (props) => {
   const { open, close } = props;
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
   const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="down" ref={ref} {...props} />;
   });
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div>
