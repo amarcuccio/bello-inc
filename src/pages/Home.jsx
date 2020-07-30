9c7d7d8ee98e9a4b820e78043a76fc58f6448f72
Andrew Marcuccio <marcuccio.andrew@gmail.com>
2020-07-30 14:44:50 -0400

    Added more content to homepage to test return to top button.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
                 <Typography variant="h6" gutterBottom>
                   Homepage
                 </Typography>
-                <Typography variant="body1">
+                <Typography variant="body1" gutterBottom>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aenean mollis elit vitae dolor tristique, in faucibus augue
                   lacinia. Donec eget arcu mattis, semper dui a, vulputate
@@ -33,6 +33,60 @@ const Home = () => {
                   non rutrum mi. Fusce pulvinar quis sapien a viverra. Mauris
                   vitae libero vitae risus malesuada viverra at ac arcu. Donec
                   rhoncus vel dui id faucibus.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Cras feugiat tincidunt dolor sed blandit. Praesent vitae arcu
+                  ac enim scelerisque ornare. Aenean sollicitudin lacus metus,
+                  sed scelerisque magna imperdiet id. In ullamcorper convallis
+                  leo vitae tristique. Vivamus vel fermentum mi. Donec varius
+                  convallis urna, a malesuada mauris tincidunt eget. Nulla a ex
+                  a augue tincidunt porttitor eu eget magna. Class aptent taciti
+                  sociosqu ad litora torquent per conubia nostra, per inceptos
+                  himenaeos. Quisque et aliquet lorem.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Proin ac lectus eleifend, gravida ex nec, sagittis nulla.
+                  Curabitur hendrerit magna nec enim ullamcorper, non posuere
+                  velit ultricies. In facilisis odio neque. Donec magna ligula,
+                  placerat sit amet odio ut, aliquam luctus justo. Quisque
+                  tempus, ante sed suscipit lacinia, eros est pulvinar urna, sed
+                  consequat nisl arcu vitae justo. Donec a gravida dolor.
+                  Pellentesque habitant morbi tristique senectus et netus et
+                  malesuada fames ac turpis egestas. Vivamus scelerisque urna ut
+                  gravida scelerisque. Vestibulum faucibus fermentum egestas.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et
+                  ultrices posuere cubilia curae; Aliquam blandit eu arcu eu
+                  placerat. Ut lacus urna, aliquam et lacinia ac, hendrerit nec
+                  erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Praesent ac lorem sed nisl rhoncus iaculis. Nullam pulvinar
+                  lobortis odio, quis elementum ex volutpat sed. Ut iaculis a
+                  mauris sed aliquam.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Phasellus et dolor pharetra, eleifend sapien id, ultricies
+                  ante. Cras mollis neque sit amet turpis sodales gravida sed
+                  sed nisi. Vivamus placerat tellus quis libero consectetur, ac
+                  sollicitudin sapien finibus. Class aptent taciti sociosqu ad
+                  litora torquent per conubia nostra, per inceptos himenaeos.
+                  Vivamus vehicula accumsan nunc sodales egestas. Praesent sit
+                  amet dolor massa. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Praesent vel nulla sit amet quam tincidunt
+                  sollicitudin vitae at dui. Sed et leo ac ipsum interdum
+                  volutpat in non mauris. Maecenas tempor, libero eu scelerisque
+                  viverra, eros justo suscipit diam, vitae ullamcorper felis
+                  magna in neque. Aliquam purus quam, consectetur sed ante non,
+                  dapibus lacinia elit.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Etiam in rutrum erat, lacinia venenatis tellus. Phasellus
+                  ultrices aliquet est et luctus. Vivamus semper pellentesque
+                  libero, at eleifend sapien pellentesque sed. Duis egestas
+                  ipsum neque, quis gravida justo dictum at. Ut augue sapien,
+                  tristique ut malesuada eu, lobortis accumsan sapien. Etiam
+                  consequat ultricies elit eget malesuada. Maecenas non interdum
+                  nunc. Curabitur ut efficitur urna. Vestibulum mollis, dui vel
+                  aliquam viverra, enim neque laoreet odio, ac convallis ipsum
+                  nisi non tellus.
                 </Typography>
               </Container>
             </Grid>
